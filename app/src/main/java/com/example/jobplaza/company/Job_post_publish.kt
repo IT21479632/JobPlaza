929d6e06ee4092d661ab2df7b9c5240f9e5d970e
sarindu <it21479632@my.sliit.lk>
2023-03-17 00:49:32 +0530

    add 2 company ui

---
+++ app/src/main/java/com/example/jobplaza/company/Job_post_publish.kt
@@ -0,0 +1,12 @@
+package com.example.jobplaza.company
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jobplaza.R
+
+class Job_post_publish : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_post_publish)
+    }
+}
